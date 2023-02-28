f9a84e77ef9b241146873d77b974aeb12feb5e97
shaikh1994 <shaikh95@live.com>
2023-02-28 23:20:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,47 @@
-from flask import Flask, request, jsonify
+import os
+from slack_bolt import App
+from slack_bolt.adapter.flask import SlackRequestHandler
+from flask import Flask, request, make_response
+from dotenv import load_dotenv
+load_dotenv()
 
+# Initialize the Flask app and the Slack app
 app = Flask(__name__)
+slack_app = App(
+    token=os.environ["SLACK_BOT_TOKEN"],
+    signing_secret=os.environ["SLACK_SIGNING_SECRET"]
+)
 
-@app.route('/hello', methods=['POST'])
-def hello():
-    return jsonify(
-        response_type='in_channel',
-        text='Hello, world!'
-    )
+# Handle incoming slash command requests
+@app.route("/slack/command", methods=["POST"])
+def handle_slash_command():
+    # Parse the command and its parameters from the request
+    command = request.form.get("command")
+    text = request.form.get("text")
 
-if __name__ == '__main__':
-    app.run()
+    # Execute the appropriate function based on the command
+    if command == "/example":
+        response_text = handle_example_command(text)
+    else:
+        response_text = "Unknown command: {}".format(command)
+
+    # Return the response to Slack
+    response = make_response(response_text)
+    response.headers["Content-type"] = "application/json"
+    return response
+
+# Define the function that handles the /example command
+def handle_example_command(text):
+    return "You entered: {}".format(text)
+
+# Start the Slack app using the Flask app as a middleware
+handler = SlackRequestHandler(slack_app)
+@app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 # from slack_bolt import App
