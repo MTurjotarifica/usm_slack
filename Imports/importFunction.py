8805901a3d8c1fa098f0ec8ccf2233efcb26202f
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-05-28 23:36:47 +0800

    Update importFunction.py

--- Imports/importFunction.py
+++ Imports/importFunction.py
@@ -3,4 +3,8 @@ from Blocks.blocks import *
 
 
 from BackgroundWorkers.trends import *
+
 from Routes.trends_route import *
+from Routes.helloUsm import *
+
+from InteractiveTrigger.interactive_trigger import *
