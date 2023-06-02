7717627ec90451f93e89f916e027ce94b54e2440
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-06-02 17:06:54 +0800

    Update trendBlock.py

--- Blocks/trendBlock.py
+++ Blocks/trendBlock.py
@@ -4,27 +4,7 @@ import numpy as np
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy import text as sqlalctext #edit st 2023-03-07
 
-def generate_trend_block():
-    engine = create_engine('mysql+pymysql://sandbox_read_only:zhsqehk23Xs8tVmVn3sSkyq5TvZumR5q@mysqldatabase.cmi5f1vp8ktf.us-east-1.rds.amazonaws.com:3306/sandbox')
-
-    #creating a connection object
-    connection = engine.connect()
-
-    #creating the metadata object
-    # metadata = MetaData()
-
-    # Loading the digital_demand table #edit pik 2023-03-07
-    # df_dd_raw_table = Table('digital_demand',
-    #                        metadata)
-
-    # This is the query to be performed #edit st 2023-03-07
-    stmt = "SELECT * FROM digital_demand WHERE (gt_category = 13) AND (country = 'DE') and (date >= '2023-01-01');" #date updated to 2022 jan 1
-
-    df_dd_raw = pd.read_sql(sqlalctext(stmt), connection) #edit st 2023-03-07
-    unique_keywords = df_dd_raw['keyword'].unique().tolist()
-        
-    connection.close()  
-
+def generate_trend_block(df):
     trend_block = [
         {
             "type": "divider"
@@ -47,9 +27,16 @@ def generate_trend_block():
             }
         }
     ]
+    # Load the Excel file into a DataFrame
+    df = pd.read_csv('unique_provider.csv')
 
+    # Now you can work with the DataFrame
+    # For example, you can print the first few rows
+    # print(df.head())
+
+    # Iterate over the DataFrame and create option dictionaries
     options = []
-    for keyword in unique_keywords:
+    for keyword in df['provider']:
         option = {
             "text": {
                 "type": "plain_text",
@@ -60,6 +47,7 @@ def generate_trend_block():
         }
         options.append(option)
 
+    # Update the options in the trend_block variable
     trend_block[1]['accessory']['options'] = options
 
     return trend_block
